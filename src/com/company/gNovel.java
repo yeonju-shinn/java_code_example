a67b16c426f62ce962ee6b62fc32e18ca3fa9a96
YeonjuS9698 <YeonjuS9698@br.spschools.org>
2022-09-26 09:08:59 -0600

    Initial commit

---
+++ src/com/company/gNovel.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class gNovel {
+    // This is the class to represent graphic novel
+
+    //Properties are the variables
+
+
+
+    ///Methods
+    // These define the behavior of the objects
+
+    // Constructor is a method
+
+    gNovel(){
+        // Constructor MUST be named the
+    }
+
+
+
+}
